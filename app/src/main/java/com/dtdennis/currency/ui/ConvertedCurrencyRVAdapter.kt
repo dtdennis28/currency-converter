1c413f17d487c70d9b0e68a2bf20fb4ea5c7046f
Drew Dennis <dennisdrew@gmail.com>
2019-11-30 18:57:33 -0600

    Working more on specialized list functionality

--- app/src/main/java/com/dtdennis/currency/ui/ConvertedCurrencyRVAdapter.kt
+++ app/src/main/java/com/dtdennis/currency/ui/ConvertedCurrencyRVAdapter.kt
@@ -1,8 +1,10 @@
 package com.dtdennis.currency.ui
 
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dtdennis.currency.R
@@ -16,6 +18,7 @@ class ConvertedCurrencyRVAdapter(
     class ConvertedCurrencyVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val currencyCodeTV = itemView.findViewById<TextView>(R.id.currency_code_tv)
         val currencyNameTV = itemView.findViewById<TextView>(R.id.currency_name_tv)
+        val conversionET = itemView.findViewById<EditText>(R.id.conversion_et)
     }
 
     override fun getItemCount() = items.size
@@ -51,11 +54,42 @@ class ConvertedCurrencyRVAdapter(
     override fun onBindViewHolder(holder: ConvertedCurrencyVH, position: Int) {
         val item = items[position]
 
-        holder.itemView.setOnClickListener {
-            onItemClickListener(position, item, holder.itemView)
-        }
-
         holder.currencyCodeTV.text = item.code
         holder.currencyNameTV.text = item.name
+        holder.conversionET.setText(String.format("%.2f", item.value))
+
+        if(position == 0) setFirstItemTouchListeners(holder)
+        else setNonFirstItemTouchListeners(position, item, holder)
+    }
+
+    private fun setFirstItemTouchListeners(holder: ConvertedCurrencyVH) {
+        holder.conversionET.setOnTouchListener(null)
+        holder.itemView.setOnClickListener(null)
+    }
+
+    private fun setNonFirstItemTouchListeners(
+        position: Int,
+        item: ConvertedCurrency,
+        holder: ConvertedCurrencyVH
+    ) {
+        // Override on-touch action-up to treat the "click" on the ET the same as an item click
+        // This will leave it so that all clicks, regardless of which child item is touched,
+        // are treated the same (e.g. to rearrange items upon click, to focus the ET, etc.)
+        holder.conversionET.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_UP) {
+                holder.itemView.performClick()
+                true
+            }
+
+            false
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener(position, item, holder.itemView)
+
+            // The only reason one would click the row would be to edit its conversion ET,
+            // So immediately request focus
+            holder.conversionET.requestFocus()
+        }
     }
 }
